4dd1da03b8c0276232be32560b76bb9e06dc02dc
Sala K 406 <salak406@salasfr.itm.edu.co>
2026-09-16 20:32:15 -0500

    Clase objetual

---
+++ productosBelleza.java
@@ -0,0 +1,45 @@
+public class productosBelleza {
+    private String nombre;
+    private String marca;
+    private double precio;
+    private String descripcion;
+
+    public productosBelleza(String nombre, String marca, double precio, String descripcion) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
